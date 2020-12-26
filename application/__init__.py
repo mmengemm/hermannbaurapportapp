b3fca51ceb5fabfbfa656d8aad7979dcbc0faa87
mmengemm <melanie.menge@protonmail.com>
2020-12-26 11:23:54 +0100

    new init app

--- application/__init__.py
+++ application/__init__.py
@@ -2,16 +2,13 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(os.environ['APP_SETTINGS'])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db = SQLAlchemy(app)
-    db.init_app(app)
-    
-    from auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-    from main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-    
-    return app
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+db.init_app(app)
+
+from auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+from main import main as main_blueprint
+app.register_blueprint(main_blueprint)

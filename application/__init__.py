a660fd9de61f3dde8dba46c9f601dca0f7f7e06d
mmengemm <melanie.menge@protonmail.com>
2020-12-26 11:22:00 +0100

    new sth

--- application/__init__.py
+++ application/__init__.py
@@ -9,9 +9,9 @@ def create_app():
     db = SQLAlchemy(app)
     db.init_app(app)
     
-    from .auth import auth as auth_blueprint
+    from auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
-    from .main import main as main_blueprint
+    from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
     return app

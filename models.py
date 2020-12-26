f3077aa8e407b7127bc05d6568efc0a44fbb187a
mmengemm <melanie.menge@protonmail.com>
2020-12-26 11:10:49 +0100

    blueprints

--- models.py
+++ models.py
@@ -1,18 +1,277 @@
+"""
+author: Melanie Menge
+date: 2020-09-02
+
+description: database models
+"""
 from app import db
-from sqlalchemy.dialects.postgresql import JSON
 
-class Result(db.Model):
-    __tablename__ = 'results'
 
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String())
-    result_all = db.Column(JSON)
-    result_no_stop_words = db.Column(JSON)
+class User(db.Model):
+    """Data model for user accounts."""
 
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
+    __tablename__ = 'login'
 
-    def __repr__(self):
-        return '<id {}>'.format(self.id)
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        nullable=False
+    )
+    username = db.Column(
+        db.String(45),
+        unique=True,
+        nullable=False
+    )
+    email = db.Column(
+        db.String(80),
+        unique=True,
+        nullable=False
+    )
+    admin = db.Column(
+        db.Boolean,
+        nullable=False
+    )
+    password = db.Column(
+        db.String(150),
+        nullable=False
+    )
+    salt = db.Column(
+        db.String(10),
+        nullable=False
+    )
+    active = db.Column(
+        db.Boolean,
+        nullable=False
+    )
+
+
+class Plz(db.Model):
+    """Data model for postcodes"""
+
+    __tablename__ = 'plz'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        nullable=False
+    )
+    plz = db.Column(
+        db.String(10),
+        nullable=False,
+        unique=True
+    )
+    ort = db.relationship(
+        'Ort',
+        backref='plz',
+        lazy=True
+    )
+
+
+
+class Ort(db.Model):
+    """Data model for towns"""
+
+    __tablename__ = 'ort'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        nullable=False
+    )
+    ort = db.Column(
+        db.String(45),
+        nullable=False,
+        unique=True
+    )
+    fk_plz = db.Column(
+        db.Integer,
+        db.ForeignKey('plz.id')
+    )
+    kunde = db.relationship(
+        'Kunde',
+        backref='ort',
+        lazy=True
+    )
+    baustelle = db.relationship(
+        'Baustelle',
+        backref='ort',
+        lazy=True
+    )
+    mitarbeiter = db.relationship(
+        'Mitarbeiter',
+        backref='ort',
+        lazy=True
+    )
+
+
+class Kunde(db.Model):
+    """Data model for clients."""
+
+    __tablename__ = 'kunden'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        nullable=False
+    )
+    firma = db.Column(
+        db.String(45),
+        unique=True,
+        nullable=False
+    )
+    ansprechsperson = db.Column(
+        db.String(45),
+        nullable=True
+    )
+    adresse = db.Column(
+        db.String(100),
+        nullable=False
+    )
+    plzort = db.Column(
+        db.Integer,
+        db.ForeignKey('ort.id')
+    )
+    email = db.Column(
+        db.String(50),
+        nullable=False
+    )
+    baustelle = db.relationship(
+        'Baustelle',
+        backref='kunden',
+        lazy=True
+    )
+    rapport = db.relationship(
+        'Rapport',
+        backref='kunden',
+        lazy=True
+    )
+
+
+class Baustelle(db.Model):
+    """Data model for building sites"""
+
+    __tablename__ = 'baustellen'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        nullable=False
+    )
+    baustelle = db.Column(
+        db.String(45),
+        nullable=False,
+        unique=True
+    )
+    fk_kunde = db.Column(
+        db.Integer,
+        db.ForeignKey('kunden.id')
+    )
+    adresse = db.Column(
+        db.String(100),
+        nullable=False
+    )
+    fk_ort = db.Column(
+        db.Integer,
+        db.ForeignKey('ort.id')
+    )
+    rapport = db.relationship(
+        'Rapport',
+        backref='baustellen',
+        lazy=True
+    )
+
+
+class Rapport(db.Model):
+    """Data model for reports"""
+
+    __tablename__ = 'rapport'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        nullable=False
+    )
+    fk_mitarbeiter = db.Column(
+        db.Integer,
+        db.ForeignKey('mitarbeiter.id'),
+        nullable=False
+    )
+    fk_kunde = db.Column(
+        db.Integer,
+        db.ForeignKey('kunden.id'),
+        nullable=False
+    )
+    fk_baustelle = db.Column(
+        db.Integer,
+        db.ForeignKey('baustellen.id'),
+        nullable=False
+    )
+    datum = db.Column(
+        db.Date,
+        nullable=False
+    )
+    stunden = db.Column(
+        db.Float,
+        nullable=False
+    )
+    arbeit = db.Column(
+        db.Text,
+        nullable=False
+    )
+    signature = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+
+class Mitarbeiter(db.Model):
+    """Data model for workers"""
+
+    __tablename__ = 'mitarbeiter'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        nullable=False
+    )
+    vorname = db.Column(
+        db.String(45),
+        nullable=False
+    )
+    nachname = db.Column(
+        db.String(45),
+        nullable=False
+    )
+    adresse = db.Column(
+        db.String(100),
+        nullable=False
+    )
+    email = db.Column(
+        db.String(45),
+        nullable=False,
+        unique=True
+    )
+    fk_ort = db.Column(
+        db.Integer,
+        db.ForeignKey('ort.id'),
+        nullable=False
+    )
+    ahv = db.Column(
+        db.String(45),
+        nullable=False,
+        unique=True
+    )
+    telnr = db.Column(
+        db.String(45),
+        nullable=False
+    )
+    gebdat = db.Column(
+        db.Date,
+        nullable=False
+    )
+    rapport = db.relationship(
+        'Rapport',
+        backref='mitarbeiter',
+        lazy=True
+    )
+

e98e129e679c947670bb10c3872233a13a944300
mmengemm <melanie.menge@protonmail.com>
2021-01-07 17:40:12 +0100

    updated imports

--- application/__init__.py
+++ application/__init__.py
@@ -12,19 +12,19 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = Config.SECRET_KEY
+db = SQLAlchemy(app)
+db.init_app(app)
 csrf = CSRFProtect(app)
 csrf.init_app(app)
 Talisman(app)
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
-db = SQLAlchemy(app)
-db.init_app(app)
 
 from .auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
+from .admin import admin as admin_blueprint
+app.register_blueprint(admin_blueprint)
 
 @login_manager.user_loader
 def load_user(id):

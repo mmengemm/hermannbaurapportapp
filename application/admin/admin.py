8ddd5fba7a456087b3bc9aa243de2d78f45430dd
mmengemm <melanie.menge@protonmail.com>
2021-01-08 07:33:28 +0100

    changed import

--- application/admin/admin.py
+++ application/admin/admin.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template	
 from application import db
 
-admin_blueprint = Blueprint('admin',__name__)
+admin = Blueprint('admin',__name__)
 
 @admin.route('/admin', methods=['GET','POST'])
 def admin():

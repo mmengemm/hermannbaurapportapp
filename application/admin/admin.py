99df449ca22653dc8814782b2c753ca2decdd13d
mmengemm <melanie.menge@protonmail.com>
2021-01-10 15:52:24 +0100

    typo

--- application/admin/admin.py
+++ application/admin/admin.py
@@ -4,6 +4,6 @@ from application import db
 admin = Blueprint('admin',__name__, template_folder='templates')
 
 @admin.route('/admin', methods=['GET','POST'])
-def admimroute():
+def adminroute():
 	return render_template('admin.html')
 

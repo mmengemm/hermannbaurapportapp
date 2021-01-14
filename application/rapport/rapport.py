0840b50e617c0ce846252e7f412bd2ef37ca9d10
mmengemm <melanie.menge@protonmail.com>
2021-01-14 08:37:33 +0100

    some changes

--- application/rapport/rapport.py
+++ application/rapport/rapport.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template	
+from flask_login import login_required
 from application import db
 from application.forms import RapportForm
 
 rapportBP = Blueprint('rapportBP',__name__, template_folder='templates')
 
 @rapportBP.route('/rapport',methods=['GET','POST'])
+@login_required
 def rapport():
     form = RapportForm()
     return render_template('rapport.html',form=form)
